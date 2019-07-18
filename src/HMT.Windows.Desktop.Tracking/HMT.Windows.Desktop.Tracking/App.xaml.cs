af8286396bfd1b2e2d06368b33e316cf9044d13a
Azmi ŞAHİN <azmisahin@outlook.com>
2019-07-18 23:01:47 +0300

    HMT Windows Desktop Tracking Application Started

---
+++ src/HMT.Windows.Desktop.Tracking/HMT.Windows.Desktop.Tracking/App.xaml.cs
@@ -0,0 +1,8 @@
+﻿namespace HMT.Windows.Desktop.Tracking {
+    using System.Windows;
+    /// <summary>
+    /// Application
+    /// </summary>
+    public partial class App : Application {
+    }
+}
---
+++ src/HMT.Windows.Desktop.Tracking/HMT.Windows.Desktop.Tracking/Properties/AssemblyInfo.cs
@@ -0,0 +1,15 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+using System.Windows;
+[assembly: AssemblyTitle("HMT.Windows.Desktop.Tracking")]
+[assembly: AssemblyDescription("Hardware Management Tool Tracking")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("azmisahin.com")]
+[assembly: AssemblyProduct("HMT.Windows.Desktop.Tracking")]
+[assembly: AssemblyCopyright("azmisahin ©  2019")]
+[assembly: AssemblyTrademark("azmisahin")]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+[assembly: ThemeInfo(ResourceDictionaryLocation.None, ResourceDictionaryLocation.SourceAssembly)]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
